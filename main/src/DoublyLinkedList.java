d648393cf63f04b7f4d4f20e7807d74df8d71d7d
Jamesg127 <jamesg127@lpsk12.org>
2026-01-20 10:04:08 -0700

    hello

--- main/src/DoublyLinkedList.java
+++ main/src/DoublyLinkedList.java
@@ -1,20 +1,98 @@
-public class DoublyLinkedList<E> implements List {
+public class DoublyLinkedList<E> implements List<E> {
     private Node<E> head;
     private int size = 0;
-    @Override
-    public void add(E value){
-        if(head == null){
-            head = new Node<>(value);
-            size++;
+    public Node<E> traverse(int i, Node<E> node){
+        if(i == 0){
+            return node;
         }
-            Node<E> newValue = new Node<>(value);
-            Node.setNext(newValue);
-        Node<E> current = (size - 1, head);
+        return traverse(i - 1, node.next);
     }
     @Override
-    public void add(int i, E element) throws IndexOutOfBoundsException;
+    public void add(E element){
+        if(head == null){
+            head = new Node<>(element);
+            size++;
+            return;
+        }
+        Node<E> newElement = new Node<>(element);
+        Node<E> current = traverse(size - 1, head);
+        size++;
+        newElement = current;
+        newElement.setPrevious(current);
+    }
+
     @Override
-    public void remove();
+    public void add(int i, E element) throws IndexOutOfBoundsException {
+        if (head == null) {
+            head = new Node<>(element);
+        }
+        if (i == 0) {
+            Node<E> newElement = new Node<>(element);
+            Node<E> otherHead = head;
+            head = newElement;
+            newElement.next = otherHead;
+            otherHead.previous = newElement;
+            return;
+        }
+        if (i < size) {
+            size++;
+            Node<E> current = traverse(i, head);
+            Node<E> newElement = new Node<>(element);
+            current.previous.next = newElement;
+            current.previous = newElement;
+            newElement.previous = current.previous;
+            newElement.next = current;
+        }
+    }
+
+
     @Override
-    public Node<E> remove(int i) throws IndexOutOfBoundsException;
+    public void remove() {
+        head = null;
+        size = 0;
+    }
+    @Override
+    public E remove(int x) {
+        if (x >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        size--;
+        Node<E> cur = traverse(x, head);
+
+
+        cur.previous.next = cur.next;
+        cur.next.previous = cur.previous;
+
+        return cur.data;
+    }
+
+
+    @Override
+    public E get(int x) {
+        return traverse(x, head).data;
+    }
+    @Override
+    public void set(int x, E element) {
+        traverse(x, head).data = element;
+    }
+    @Override
+    public int size() {
+        return size;
+    }
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public String toString(){
+        String result = "";
+        if(head != null) {
+            result += head.data;
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+                result += current.data + ", ";
+            }
+        }
+        return result;
+    }
 }
--- main/src/Node.java
+++ main/src/Node.java
@@ -1,18 +1,9 @@
 public class Node<E> {
-    private E data;
-    public static Node<E> next;
-    public static Node<E> previous;
-    public Node(E d){
+    public E data;
+    public  Node<E> next;
+    public  Node<E> previous;
+    public Node(E d) {
         data = d;
-    }
-    public E getValue(){
-        return data;
-    }
-    public void setValue(E v) {
-        data = v;
-    }
-    public static void setNext(Node<E> n){
-        next = n;
     }
     public void setPrevious(Node<E> p){
         previous = p;
@@ -21,13 +12,5 @@ public class Node<E> {
     public String toString(){
         String string = data.toString();
         return string;
-    }
-    public boolean equals(Node<E> node){
-        if(node.equals(data)){
-            return true;
-        }
-        else{
-            return false;
-        }
     }
 }
