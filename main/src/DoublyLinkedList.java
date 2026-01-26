a4836a6849c22a7245911e7e8d4918e32aaea434
Jamesg127 <jamesg127@lpsk12.org>
2026-01-26 11:01:53 -0700

    je suis un croissant

--- main/src/DoublyLinkedList.java
+++ main/src/DoublyLinkedList.java
@@ -1,98 +1,113 @@
 public class DoublyLinkedList<E> implements List<E> {
-    private Node<E> head;
     private int size = 0;
-    public Node<E> traverse(int i, Node<E> node){
-        if(i == 0){
+    private Node<E> head;
+
+    private Node<E> traverse(int i, Node<E> node) {
+        if(i == 0) {
             return node;
         }
-        return traverse(i - 1, node.next);
+        return traverse(i - 1, node.getNext());
     }
-    @Override
-    public void add(E element){
-        if(head == null){
+
+    public void add(E element) {
+        if(size == 0) {
             head = new Node<>(element);
             size++;
             return;
         }
-        Node<E> newElement = new Node<>(element);
-        Node<E> current = traverse(size - 1, head);
         size++;
-        newElement = current;
-        newElement.setPrevious(current);
+
+        Node<E> newElem = new Node<>(element);
+        Node<E> cur = traverse(size - 2, head);
+
+        cur.setNext(newElem);
+        newElem.setPrevious(cur);
     }
 
     @Override
-    public void add(int i, E element) throws IndexOutOfBoundsException {
+    public void add(int i, E element) {
         if (head == null) {
             head = new Node<>(element);
         }
         if (i == 0) {
-            Node<E> newElement = new Node<>(element);
-            Node<E> otherHead = head;
-            head = newElement;
-            newElement.next = otherHead;
-            otherHead.previous = newElement;
+            Node<E> newElem = new Node<>(element);
+            Node<E> oldHead = head;
+            head = newElem;
+            newElem.setNext(oldHead);
+            oldHead.setPrevious(newElem);
             return;
         }
-        if (i < size) {
-            size++;
-            Node<E> current = traverse(i, head);
-            Node<E> newElement = new Node<>(element);
-            current.previous.next = newElement;
-            current.previous = newElement;
-            newElement.previous = current.previous;
-            newElement.next = current;
+        if (i >= size) {
+            throw new IndexOutOfBoundsException();
         }
-    }
+        size++;
 
+        Node<E> cur = traverse(i, head);
+
+        Node<E> newElem = new Node<>(element);
+
+        cur.getPrevious().setNext(newElem);
+        cur.setPrevious(newElem);
+
+        newElem.setPrevious(cur.getPrevious());
+        newElem.setNext(cur);
+    }
 
     @Override
     public void remove() {
         head = null;
         size = 0;
     }
+
     @Override
-    public E remove(int x) {
-        if (x >= size) {
+    public E remove(int i) {
+        if (i >= size) {
             throw new IndexOutOfBoundsException();
         }
         size--;
-        Node<E> cur = traverse(x, head);
 
+        Node<E> cur = traverse(i, head);
 
-        cur.previous.next = cur.next;
-        cur.next.previous = cur.previous;
+        cur.getPrevious().setNext(cur.getNext());
+        cur.getNext().setPrevious(cur.getPrevious());
 
-        return cur.data;
+        return cur.getData();
     }
-
 
     @Override
-    public E get(int x) {
-        return traverse(x, head).data;
+    public E get(int i) {
+        return traverse(i, head).getData();
     }
+
     @Override
-    public void set(int x, E element) {
-        traverse(x, head).data = element;
+    public void set(int i, E element) {
+        traverse(i, head).setData(element);
     }
+
     @Override
     public int size() {
         return size;
     }
+
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
-    public String toString(){
+
+    @Override
+    public String toString() {
         String result = "";
+
         if(head != null) {
-            result += head.data;
-            Node<E> current = head;
-            while (current.next != null) {
-                current = current.next;
-                result += current.data + ", ";
+            result += head.getData();
+
+            Node<E> cur = head;
+            while (cur.getNext() != null) {
+                cur = cur.getNext();
+                result += cur.getData() + ", ";
             }
         }
+
         return result;
     }
 }
--- main/src/Node.java
+++ main/src/Node.java
@@ -1,13 +1,29 @@
 public class Node<E> {
-    public E data;
-    public  Node<E> next;
-    public  Node<E> previous;
+    private E data;
+    private  Node<E> next;
+    private  Node<E> previous;
+    public E getData(){
+        return data;
+    }
+    public Node<E> getNext(){
+        return next;
+    }
+    public Node<E> getPrevious(){
+        return previous;
+    }
     public Node(E d) {
         data = d;
     }
     public void setPrevious(Node<E> p){
         previous = p;
     }
+    public void setNext(Node<E> n){
+        next = n;
+    }
+    public void setData(E d){
+        data = d;
+    }
+
 
     public String toString(){
         String string = data.toString();
